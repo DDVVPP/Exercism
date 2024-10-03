56a9db4d66bf8c5d44648c6d673d0de4e936926d
DDVVPP <darshinvp@gmail.com>
2024-10-03 12:26:33 -0700

    update: passing tests

--- javascript/elyses-destructured-enchantments/enchantments.js
+++ javascript/elyses-destructured-enchantments/enchantments.js
@@ -8,8 +8,12 @@
  *
  * @returns {Card} the first card in the deck
  */
+
+// Without using .shift() or array-index]
+
 export function getFirstCard(deck) {
-  throw new Error('Implement the getFirstCard function');
+  const [firstCard, ...rest] = deck;
+  return firstCard;
 }
 
 /**
@@ -20,7 +24,8 @@ export function getFirstCard(deck) {
  * @returns {Card} the second card in the deck
  */
 export function getSecondCard(deck) {
-  throw new Error('Implement the getSecondCard function');
+  const [firstCard, secondCard, ...rest] = deck;
+  return secondCard;
 }
 
 /**
@@ -31,7 +36,8 @@ export function getSecondCard(deck) {
  * @returns {Card[]} new deck with reordered cards
  */
 export function swapTopTwoCards(deck) {
-  throw new Error('Implement the swapTopTwoCards function');
+  const [firstCard, secondCard, ...rest] = deck;
+  return [secondCard, firstCard, ...rest];
 }
 
 /**
@@ -43,11 +49,12 @@ export function swapTopTwoCards(deck) {
  * deck and a new deck containing all the other cards
  */
 export function discardTopCard(deck) {
-  throw new Error('Implement the discardTopCard function');
+  const [firstCard, ...rest] = deck;
+  return [firstCard, [...rest]];
 }
 
 /** @type {Card[]} **/
-const FACE_CARDS = ['jack', 'queen', 'king'];
+const FACE_CARDS = ["jack", "queen", "king"];
 
 /**
  * Insert face cards into the given deck
@@ -58,5 +65,7 @@ const FACE_CARDS = ['jack', 'queen', 'king'];
  * third, and fourth cards are the face cards
  */
 export function insertFaceCards(deck) {
-  throw new Error('Implement the insertFaceCards function');
+  const [firstCard, ...rest] = deck;
+  const newDeck = [firstCard, "jack", "queen", "king", ...rest];
+  return newDeck;
 }
